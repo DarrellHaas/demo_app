41c7061bcc81afa934d9dd146f6051c39526563a
Darrell Haas <darrell@lightning-bolt.com>
2014-05-07 13:13:35 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '71314e568eaa056f8ec591fa7a447096cece14ca757f1054992c124be2d1c67632689d858e4840b66251273a072cc8fae4e2b5d648f63b3819a2f2d4cc70777b'
